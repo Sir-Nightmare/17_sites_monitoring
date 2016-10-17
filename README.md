46a1f06b1019afb6585e97f916d38a2301a61b35
Sir Nightmare <ivantochilkin@gmail.com>
2016-10-18 00:42:34 +0300

    the task was done

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 17_sites_monitoring
+This script shows following information about sites from the list:
+* Does server respond with "200" 
+* Domain expiration date
+* Is domain paid for month ahead 
+
+You will need **requests** and **pytoth-whois** packages to run this script.
+
+You can run the script using following command: _python check_sites_health.py \<path_to_file_with_urls\>_
+
+Urls have to be like: http://example.com/.
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,55 @@
+import requests
+from whois import whois
+from datetime import datetime, timedelta
+
+PAID_DAYS_CHECK = 30
+
+
 def load_urls4check(path):
-    pass
+    with open(path, 'r', encoding="utf8") as input_file:
+        url_list = input_file.read().split()
+    return url_list
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        server_answer = requests.get(url)
+        if server_answer.status_code == 200:
+            return 'Yes'
+    except requests.ConnectionError:
+        pass
+    return 'No'
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    site_info = whois(url)
+    if type(site_info['expiration_date']) == list:
+        return site_info['expiration_date'][0]
+    return site_info['expiration_date']
+
+
+def is_domain_paid_month_ahead(expiration_date):
+    if expiration_date - datetime.today().date() >= timedelta(days=PAID_DAYS_CHECK):
+        return 'Yes'
+    return 'No'
+
 
 if __name__ == '__main__':
-    pass
+    url_list = load_urls4check('urls.txt')
+    print('{0:40s}{4:5}{1:30s}{4:5}{2:25s}{4:5}{3:35s}{4:5}'.format('Site url', 'Does server respond with "200"',
+                                                                    'Domain expiration date',
+                                                                    'Is domain paid for month ahead', '  |  '))
+    print('-' * 148)
+    for url in url_list:
+        is_responding = is_server_respond_with_200(url)
+        expiration_date = get_domain_expiration_date(url)
+        is_paid = 'No info'
+        if expiration_date:
+            expiration_date = expiration_date.date()
+            is_paid = is_domain_paid_month_ahead(expiration_date)
+
+        else:
+            expiration_date = 'No info'
+        print(
+            '{0:40s}{4:5}{1:30s}{4:5}{2:25s}{4:5}{3:35s}{4:5}'.format(url, is_responding,
+                                                                      str(expiration_date), is_paid, '  |  '))
